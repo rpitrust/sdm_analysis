1db18ede4cacdaef774184773b33c4670bfaf974
jbakdash <jbakdash@gmail.com>
2014-07-21 15:07:03 -0400

    Added software version info

--- R_analysis_script_T1.R
+++ R_analysis_script_T1.R
@@ -1,5 +1,7 @@
 # Jon's R code for T1 analysis
 # 7/21/2014
+# R version 3.1.0 
+# RStudio Version 0.98.953
 # Zip file: CSVs final clean 4.22.14.zip
 # 148 csv files; 10 unusable csv files (12, 24, 31, 36, 53, 57, 75, 80, 106, 143) placed in a separate directory
 
@@ -26,7 +28,7 @@ read.csvfiles<-function(filenam)
   {read.csv(filenam, 
             header = FALSE,
             col.names=paste(colnames(CSVHeaders), sep =""), #Add a header to each file, makes rbind work!
-            stringsAsFactors=FALSE #Fix for the mountains of errors  because of mixed numeric and text data: http://stackoverflow.com/questions/1632772/appending-rows-to-a-dataframe-the-factor-problem
+            stringsAsFactors=FALSE #Fix for the mountains of errors because of mixed numeric and text data: http://stackoverflow.com/questions/1632772/appending-rows-to-a-dataframe-the-factor-problem
             )
    }
 
